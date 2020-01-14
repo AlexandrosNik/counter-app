9bdfda374585ae3ad0c4e811455c98188237449c
Nikoletopoulos <alexandros.nikoletopoulos@atos.net>
2020-01-14 17:03:27 +0200

    added delete btn and multiple counter components

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,31 +2,46 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0
+        value: this.props.counter.value
     }
 
     handleIncrement = () => {
-        this.setState({count: this.state.count + 1})
+        this.setState({value: this.state.value + 1})
     }
 
     render() { 
         return ( 
         <div>
-            <span className = {this.getClasses()}>{this.formatCount()}</span>
-            <button className = 'btn btn-secondary btn-sm' onClick = {this.handleIncrement}>Increment</button>
+            <span 
+            className = {this.getClasses()}
+            > 
+            {this.formatCount()} 
+            </span>
+            <button 
+            className = 'btn btn-secondary btn-sm' 
+            onClick = {this.handleIncrement}
+            >
+            Increment
+            </button>
+            <button 
+            className="btn btn-danger btn-sm m-2" 
+            onClick = {() => this.props.onDelete(this.props.counter.id)}
+            >
+            Delete
+            </button>
         </div> 
         );
     }
 
     getClasses() {
         let classes = 'badge m-2 badge-';
-        classes += (this.state.count === 0) ? 'warning' : 'primary';
+        classes += (this.state.value === 0) ? 'warning' : 'primary';
         return classes;
     }
 
     formatCount() {
-        const { count } = this.state;
-        return count === 0 ? 'Zero' : count;
+        const { value } = this.state;
+        return value === 0 ? 'Zero' : value;
     }
 }
  
